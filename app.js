05c0a7979ecec9338fdda313bd1a81425cec3e0b
MD Ripon Islam <islamripon876@gmail.com>
2021-03-21 22:55:21 +0600

    index page added

--- app.js
+++ app.js
@@ -1,25 +1,85 @@
 var dotenv        = require('dotenv').config();
 var express       = require("express");
 var mongoose      = require("mongoose");
+var User          = require("./models/user");
 var passport      = require("passport");
 var localStrategy = require("passport-local");
 var bodyParser    = require("body-parser");
 var nodeMailer    = require("nodemailer");
+var fs            = require('fs');
+var path          = require('path');
 var port          = process.env.PORT || 5000;
 
-// database configuration
+// database - configuration
 mongoose.connect("mongodb://localhost:27017/eod", {useUnifiedTopology: true, useNewUrlParser: true});
 mongoose.set('useFindAndModify', false);
+
+// main - configuration
 
 var app = express();
 app.set("view engine","ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// passport / authentication - configuration
+app.use(require('express-session')({ secret: "My name is MD Ripon Islam", resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
 
 
 
 
 app.get("/",function(req,res){
 	res.render("index");
+});
+
+app.post("/register",function(req,res){
+  var Rtitle = "EOD| Home"
+	var newUser = new User({username: req.body.username,name: req.body.name});
+      User.register(newUser,req.body.password,function(err,user){
+      	if(err){
+      		console.log(err);
+      		res.render("/",{title: Rtitle})
+      	}else{
+      		console.log(user);
+      		passport.authenticate("local")(req,res,function(){
+            
+            	fs.mkdir('public/uploads/' + req.user.username, function(err){
+              if (err) {
+                  console.log(err);
+                };
+                console.log("Directory is created.");
+              
+                     });
+             if (req.body.username == "ripon") {
+
+              var doAuthor = {isAdmin: true};
+            User.findByIdAndUpdate(req.user._id,doAuthor,{new: true},function(err,user){
+                   if (err) {
+                    console.log(err);
+                   }else{
+                      console.log(user);
+                      res.redirect("/admin");
+                   };
+                 });
+             }else{
+              res.redirect("/");
+             };
+             
+      		});
+      	};
+      });
+
 });
 
 app.listen(port,function(){
---
+++ models/user.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var userSchema = new mongoose.Schema({
+	name: String,
+	username: String,
+	password: String,
+    age: Number,
+    hairColour: String,
+    cupSize: String,
+    dressSize: String,
+    eyeColour: String,
+    height: String,
+    nationality: String,
+    location: String,
+    area: String,
+    oneHourRate: String,
+    twoHourRate: String,
+    threeHourRate: String,
+    liaiseWith:[{ type: String }],
+    thingWeCanDo: String,
+    email: String,
+    phoneNumber: Number,
+    websiteUrl: String,
+    PreferredContactMethod: String,
+    avilable: { type: Boolean, default: false },
+    instagramUrl: String,
+	isUser: { type: Boolean, default: true },
+	isAdmin: { type: Boolean, default: false }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+var User = mongoose.model("User",userSchema);
+
+module.exports = User;
